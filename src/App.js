ae5d00932f244771fc407d194dc10318d047d94d
Vlad160116 <ss.tymoshenko.vlad123@gmail.com>
2022-02-12 23:37:51 +0300

    empty

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-// import React from 'react';
-//
-// import './App.css';
-// import Notebook from "./components/Notebook";
-//
-// function App() {
-//     return (
-//         // <Notebook/>
-//         <Users/>
-//     );
-// };
-//
-// export default App;
+import React from 'react';
 
-
-
-// Hw1
-import './App.css';
-
-
-function App() {
-
-    let [users, setUsers] = useState([]);
-    let [users, setUsers] = useState([])
-    let [users, setUsers] = useState([])
-
-
+const App = () => {
     return (
         <div>
-
+            
         </div>
     );
 };
 
 export default App;
-
